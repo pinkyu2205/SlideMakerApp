77385f86900a3abd8c342056b1a3ce4cdcaa9429
Nguyễn Hoàng Long 40 <longnhse172340@fpt.edu.vn>
2025-11-17 21:46:24 +0700

    add CurriculumManagementPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Footer from './components/Layout/Footer/Footer'
 import Header from './components/Layout/Header/Header'
 import AdminDashboardPage from './pages/Admin/AdminDashboardPage'
 import AdminLayout from './pages/Admin/AdminLayout'
+import CurriculumManagementPage from './pages/Admin/CurriculumManagementPage'
 import UserManagementPage from './pages/Admin/UserManagementPage'
 import Login from './pages/AuthPage/Login/Login'
 import Register from './pages/AuthPage/Register/Register'
 import DashboardPage from './pages/DashboardPage/DashboardPage'
 import HomePage from './pages/HomePage/HomePage'
 import OptionsTemplatePage from './pages/OptionsTemplatePage/OptionsTemplatePage'
+import SlideGeneratorPage from './pages/SlideGeneratorPage/SlideGenerator'
 import TemplateLibraryPage from './pages/TemplateLibraryPage/TemplateLibraryPage'
 export default function App() {
   const location = useLocation()
@@ -19,17 +21,17 @@ export default function App() {
   const showFooter = currentPage === '/' && !isAdminPage
 
   return (
-    <div className="app-container">
+    <div className='app-container'>
       <Header currentPage={currentPage} />
-      <main className="main-content">
+      <main className='main-content'>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/templates" element={<TemplateLibraryPage />} />
-          <Route path="/options-template" element={<OptionsTemplatePage />} />
-          <Route path="/slide-generator" element={<SlideGeneratorPage />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/dashboard' element={<DashboardPage />} />
+          <Route path='/templates' element={<TemplateLibraryPage />} />
+          <Route path='/options-template' element={<OptionsTemplatePage />} />
+          <Route path='/slide-generator' element={<SlideGeneratorPage />} />
         </Routes>
 
         <Routes element={<AdminRoute />}>
@@ -37,10 +39,11 @@ export default function App() {
             <Route index element={<Navigate to='/admin/dashboard' replace />} />
             <Route path='dashboard' element={<AdminDashboardPage />} />
             <Route path='users' element={<UserManagementPage />} />
+            <Route path='curriculum' element={<CurriculumManagementPage />} />
           </Route>
         </Routes>
       </main>
       {showFooter && <Footer />}
     </div>
-  );
+  )
 }
